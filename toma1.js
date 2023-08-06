f14146ed2f414a689fae775deab217f53e440cb1
Toma176373 <105673400+Toma176373@users.noreply.github.com>
2023-08-06 16:56:31 +0200

    upload init files
    A not working webpage.

---
+++ toma1.js
@@ -0,0 +1,7 @@
+/* function named toma1 */
+
+function toma1() {
+	var t1l = getElementById("toma1list").value;
+	var t1d = getElementById("toma1div").value;
+	getElementbyId(t1d).style.t1l = getElementbyId(toma1color).value;
+}
