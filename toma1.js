fa70c8ccbfda6d5b6338f3cf8fe2a7dad3f39593
Mormota <mormotadevelop@gmail.com>
2023-08-08 18:58:17 +0200

    The select tags got label. Js code change to if.

--- toma1.js
+++ toma1.js
@@ -3,5 +3,9 @@
 function toma1() {
 	var t1l = document.getElementById("toma1list").value;
 	var t1d = document.getElementById("toma1div").value;
-	document.getElementbyId(t1d).style.t1l = document.getElementbyId(toma1color).value;
+	if (t1l == "background")
+		document.getElementById(t1d).style.background = document.getElementById("toma1color").value;
+	else
+		document.getElementById(t1d).style.color = document.getElementById("toma1color").value;
+	// document.getElementbyId(t1d).style.t1l = document.getElementbyId(toma1color).value;
 }
